b39e2f20e3e92a89695ce6451bfa7d561da1557d
navya-vithanala <148491748+navya-vithanala@users.noreply.github.com>
2025-09-24 22:39:50 -0700

    Update CredentialNFT.sol

--- ethereum/CredentialNFT.sol
+++ ethereum/CredentialNFT.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 contract MyNFT is ERC721 {
     string public message;
-    constructor() ERC721("pure-buck", "CRED") {
+
+    constructor() ERC721("pure-buck", "CUC") {
         message = "A decentralized hash with 5 blocks - nvithana";
     }
 }
